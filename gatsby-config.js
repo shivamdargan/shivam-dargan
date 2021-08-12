87dbc885693e6bee4d4ff6a8ba9d2ebfc588240b
shivamdargan <darganshivam2001@gmail.com>
2021-08-13 04:55:50 +0530

    Commented Gatsby Config Files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,32 @@
 module.exports = {
-  plugins: [
-    // ...
-    {
-      resolve: 'gatsby-plugin-load-script',
-      options: {
-        src: './static/modernizr.js', // Change to the script filename
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-load-script',
-      options: {
-        src: './static/pace.min.js', // Change to the script filename
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-load-script',
-      options: {
-        src: './static/jquery-3.2.1.min.js', // Change to the script filename
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-load-script',
-      options: {
-        src: './static/main.js', // Change to the script filename
-      },
-    },
-  ],
+  // plugins: [
+  //   // ...
+  //   {
+  //     resolve: 'gatsby-plugin-load-script',
+  //     options: {
+  //       src: './static/modernizr.js', // Change to the script filename
+  //     },
+  //   },
+  //   {
+  //     resolve: 'gatsby-plugin-load-script',
+  //     options: {
+  //       src: './static/pace.min.js', // Change to the script filename
+  //     },
+  //   },
+  //   {
+  //     resolve: 'gatsby-plugin-load-script',
+  //     options: {
+  //       src: './static/jquery-3.2.1.min.js', // Change to the script filename
+  //     },
+  //   },
+  //   {
+  //     resolve: 'gatsby-plugin-load-script',
+  //     options: {
+  //       src: './static/main.js', // Change to the script filename
+  //     },
+  //   },
+  // ],
+  
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "portfolio-website",
