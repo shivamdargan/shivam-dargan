29c324dc768e30eff8454c7ad0e5182465aaff69
shivamdargan <darganshivam2001@gmail.com>
2021-08-13 16:26:36 +0530

    Commented Gatsby Config Files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,17 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  // plugins: [
-  //   // ...
-  //   {
-  //     resolve: 'gatsby-plugin-load-script',
-  //     options: {
-  //       src: './static/modernizr.js', // Change to the script filename
-  //     },
-  //   },
-  //   {
-  //     resolve: 'gatsby-plugin-load-script',
-  //     options: {
-  //       src: './static/pace.min.js', // Change to the script filename
-  //     },
-  //   },
-  //   {
-  //     resolve: 'gatsby-plugin-load-script',
-  //     options: {
-  //       src: './static/jquery-3.2.1.min.js', // Change to the script filename
-  //     },
-  //   },
-  //   {
-  //     resolve: 'gatsby-plugin-load-script',
-  //     options: {
-  //       src: './static/main.js', // Change to the script filename
-  //     },
-  //   },
-  // ],
-  
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "portfolio-website",
   },
   plugins: ['html-webpack-plugin',
+            {
+              resolve: `gatsby-plugin-styled-components`,
+              options: {
+                // Add any options here
+              },
+            },
             "gatsby-plugin-gatsby-cloud",
             "gatsby-plugin-image",
             "gatsby-plugin-sharp",
