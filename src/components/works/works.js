ee36376251ecdebd9c987d9593179b3285f3d6b2
shivamdargan <darganshivam2001@gmail.com>
2021-08-17 03:51:53 +0530

    Added 3 projects To projects Section

--- src/components/works/works.js
+++ src/components/works/works.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { StaticImage } from 'gatsby-plugin-image'
-
+import ctf from "../../../static/CTF.jpg"
+import coviRes from "../../../static/covi-res.jpg"
+import Register from "../../../static/Register.jpg"
 export default function Works() 
 {
-
   return(
     <div>
          <section id="works" className="s-works target-section">
@@ -26,24 +27,32 @@ export default function Works()
         <div className="masonry__brick" data-aos="fade-up">
             <div className="item-folio">
                 <div className="item-folio__thumb">
-                    <a href="images/portfolio/gallery/g-city-building.jpg" className="thumb-link" title="Shutterbug" data-size="1050x700">
-                        <StaticImage src="../../assets/images/portfolio/city-building.jpg"
+                    <a href={ctf} className="thumb-link" title="CTF Portal" data-size="2000x1600">
+                        <StaticImage src="../../assets/images/portfolio/ctfCollage.png"
                               alt=""/>
                     </a>    
-                    <a href="https://www.behance.net/" className="item-folio__project-link" title="Project link" target="_blank"></a>
+                    <a href="https://drive.google.com/file/d/16PqOoQ90dFClw8INbkP-I5lT8X8FUScr/view?usp=sharing" className="item-folio__project-link" title="Project link" target="_blank"></a>
                 </div>
 
                 <div className="item-folio__text">
                     <h4 className="item-folio__title">
-                        City Building
+                        Capture The Flag (CTF) Portal
                     </h4>
                     <p className="item-folio__cat">
-                        Branding
+                        Marvel Themed
                     </p>
+                    <br/>
+                    <b>Tech Stack Used :</b> <span style={{color:"#CF1867"}}>React.js | Express.js, Node.js | Mongo DB</span>
                 </div>
 
                 <div className="item-folio__caption">
-                    <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
+                    <p>The Portal Carried A Marvel Theme With It Thus Having These Various Unique Features And Suspence Built Right Into It :<br/>
+A) 6 Infinity Stones - Each Stone Carried A Unique Power With It, For Example, Power Stone Gave A User 5 Free Hints, Soul Stone Gave The Ability To A Team To Deduct 10% Of 
+     Points Of A Team Of Their Choice From The Live Points LeaderBoard Unless If A Team Had Activated Reality Stone Which Made That Particular Team Hidden From The 
+     LeaderBoard And Thus Invincible And So On...<br/>
+B) The Stones Had To Be Earned By Solving All The Technical Marvel Themed Questions Of A Round In A Given Time Frame<br/>
+C) A Secret Was Kept Just Until The End Known As The Snap i.e If A Team Has All The 6 Stones And Have Finished All The Rounds, A Gauntlet Would Have Appeared And By 
+     Clicking It Would Subsequently Trigger A Snap And Immediately Make That Particular Team Win!<br/> <span style={{color:"#CF1867"}}> Please Download The Image Using The Button On The Top Right Corner If It Is Not Clearly Visible.<br/>Contact Me For Code Base</span><br/>Corresponding Link For The Project Is Given At Project Tile.</p>
                 </div>
             </div>
         </div> 
@@ -51,22 +60,23 @@ export default function Works()
         <div className="masonry__brick" data-aos="fade-up">
             <div className="item-folio">
                 <div className="item-folio__thumb">
-                    <a href="images/portfolio/gallery/g-woodcraft.jpg" className="thumb-link" title="Woodcraft" data-size="1050x700">
-                        <StaticImage src="../../assets/images/portfolio/woodcraft.jpg" 
+                    <a href={coviRes} className="thumb-link" title="Covid Resources" data-size="2000x1600">
+                        <StaticImage src="../../assets/images/coviRes.png" 
                              alt=""/>
                     </a>
-                    <a href="https://www.behance.net/" className="item-folio__project-link" title="Project link" target="_blank"></a>
+                    <a href="https://github.com/shivamdargan/Covid-Resources" className="item-folio__project-link" title="Project link" target="_blank"></a>
                 </div>
                 <div className="item-folio__text">
                     <h4 className="item-folio__title">
-                        Woodcraft
+                        Covi Res
                     </h4>
                     <p className="item-folio__cat">
-                        Web Design
+                        A State Of The Art Covid Resources Website
                     </p>
+                    <br/><b>Tech Stack Used :</b> <span style={{color:"#CF1867"}}>React.js | Express.js, Node.js</span>
                 </div>
                 <div className="item-folio__caption">
-                    <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
+                    <p>Covid Resources Website Which Provides The Latest Resources Verified By Your Selection Of Preferred City And Resource.<span style={{color:"#CF1867"}}> <br/>Please Download The Image Using The Button On The Top Right Corner If It Is Not Clearly Visible.</span><br/>Corresponding Link For The Project Is Given At Project Tile.</p>
                 </div>
             </div>
         </div> 
@@ -75,22 +85,23 @@ export default function Works()
             <div className="item-folio">
                     
                 <div className="item-folio__thumb">
-                    <a href="images/portfolio/gallery/g-beetle.jpg" className="thumb-link" title="The Beetle Car" data-size="1050x700">
-                        <StaticImage src="../../assets/images/portfolio/the-beetle.jpg"
+                    <a href={Register} className="thumb-link" title="Registration Portal" data-size="2000x1600">
+                        <StaticImage src="../../assets/images/RegistrationPortal.png"
                              alt=""/>
                     </a>
-                    <a href="https://www.behance.net/" className="item-folio__project-link" title="Project link" target="_blank"></a>
+                    <a href="https://jarvis.owasp.co.in/" className="item-folio__project-link" title="Project link" target="_blank"></a>
                 </div>
                 <div className="item-folio__text">
                     <h4 className="item-folio__title">
-                        The Beetle
+                        Registration Portal
                     </h4>
                     <p className="item-folio__cat">
-                        Web Development
+                       For A Technical Event
                     </p>
+                    <br/><b>Tech Stack Used :</b> <span style={{color:"#CF1867"}}>React.js | Express.js, Node.js | Mongo DB</span>
                 </div>
                 <div className="item-folio__caption">
-                    <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
+                <p>A Fully Fledged Registration Portal Based On MERN Stack For A Technical Event Capable Of Handling More Than 900+ Registrations. <span style={{color:"#CF1867"}}> <br/>Please Download The Image Using The Button On The Top Right Corner If It Is Not Clearly Visible.</span><br/>Corresponding Link For The Project Is Given At Project Tile.</p>
                 </div>
             </div>
         </div> 
@@ -111,6 +122,7 @@ export default function Works()
                     <p className="item-folio__cat">
                         Branding
                     </p>
+                    <br/><b>Tech Stack Used :</b> <span style={{color:"#CF1867"}}>React.js | Express.js, Node.js | Mongo DB</span>
                 </div>
                 <div className="item-folio__caption">
                     <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
@@ -135,6 +147,7 @@ export default function Works()
                     <p className="item-folio__cat">
                         Web Design
                     </p>
+                    <br/><b>Tech Stack Used :</b> <span style={{color:"#CF1867"}}>React.js | Express.js, Node.js | Mongo DB</span>
                 </div>
                 <div className="item-folio__caption">
                     <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
@@ -159,6 +172,7 @@ export default function Works()
                     <p className="item-folio__cat">
                         Web Design
                     </p>
+                    <br/><b>Tech Stack Used :</b> <span style={{color:"#CF1867"}}>React.js | Express.js, Node.js | Mongo DB</span>
                 </div>
                 <div className="item-folio__caption">
                     <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
